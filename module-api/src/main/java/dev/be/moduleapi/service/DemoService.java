c3d497f4a7f52943dd0d8f0149e99e22f52f6b74
sjh9391985 <sjh9391985@gmail.com>
2024-01-20 14:16:33 +0900

    feat: "mysql 라이브러리 추가"

--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,7 +1,9 @@
 package dev.be.moduleapi.service;
 
 import dev.be.moduleapi.exception.CustomException;
+import dev.be.modulecommon.domain.Member;
 import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.repository.MemberRepository;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,12 +16,17 @@ public class DemoService {
 
     private final CommonDemoService commonDemoService;
 
+    private final MemberRepository memberRepository;
     public String save(){
-        log.info(commonDemoService.commonService());
+        memberRepository.save(Member.builder()
+            .name(Thread.currentThread().getName())
+            .build());
         return "save";
     }
 
     public String find(){
+        int size = memberRepository.findAll().size();
+        log.info("size : {}", size);
         return "find";
     }
 
