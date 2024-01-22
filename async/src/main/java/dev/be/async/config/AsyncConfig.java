afec969d5e524dd974f76ef3a33c06eb799f19a6
sjh9391985 <sjh9391985@gmail.com>
2024-01-22 21:18:23 +0900

    feat: "비동기 config 설정"

---
+++ async/src/main/java/dev/be/async/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package dev.be.async.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+
+@Configuration
+@Async
+public class AsyncConfig {
+}
