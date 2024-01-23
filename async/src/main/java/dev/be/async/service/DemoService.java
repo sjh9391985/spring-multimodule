922c8b6143c085b639470678616a47f672ed1490
sjh9391985 <sjh9391985@gmail.com>
2024-01-23 21:21:44 +0900

    feat: "feign service 추가"

---
+++ async/src/main/java/dev/be/async/service/DemoService.java
@@ -0,0 +1,21 @@
+package dev.be.async.service;
+
+import dev.be.async.common.dto.BaseRequestInfo;
+import dev.be.async.common.dto.BaseResponseInfo;
+import dev.be.async.feign.client.DemoFeignClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DemoService {
+
+    private final DemoFeignClient demoFeignClient;
+
+    public String get() {
+        ResponseEntity<BaseResponseInfo> response = demoFeignClient.callGet("CustomHeader", "CustomName", 1L);
+        return "get";
+    }
+
+}
