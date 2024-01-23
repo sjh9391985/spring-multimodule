16a7047c8959ac58336b29b99be229278dd2de08
sjh9391985 <sjh9391985@gmail.com>
2024-01-23 21:20:30 +0900

    feat: "요청, 응답 모델 추가"

---
+++ async/src/main/java/dev/be/async/common/dto/BaseRequestInfo.java
@@ -0,0 +1,17 @@
+package dev.be.async.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) // Null 값은 제외
+public class BaseRequestInfo {
+    private String name;
+    private Long age;
+}
